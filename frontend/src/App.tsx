893cbf060f35b721533f81028d1e2a6e067a98c1
AdamPostMontjoie <57641108+AdamPostMontjoie@users.noreply.github.com>
2025-11-01 17:04:27 -0400

    vite tailwind syncfusion

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import MarkerTemplateMaps from './components/map'
+
+function App() {
+
+  return (
+    <div className="text-blue-500 text-xl font-bold">
+     Hi
+      <MarkerTemplateMaps/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/map.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { MapsComponent, LayersDirective, LayerDirective } from '@syncfusion/ej2-react-maps';
+
+// Assumes you moved this to 'src/maps/australia.json'
+import * as australia from '../maps/australia.json'
+
+const SimpleMap = () => {
+    return (
+        // Make sure this component has a defined height, 
+        // e.g., by giving its parent div a class like 'h-screen'
+        <MapsComponent id="maps" zoomSettings={{ enable: true }}>
+            <LayersDirective>
+                <LayerDirective 
+                    shapeData={australia}
+                    // 'shapeSettings' is what gives the map its color
+                    shapeSettings={{ 
+                        autofill: true,
+                        border: { width: 0.5, color: 'white' } 
+                    }} 
+                />
+            </LayersDirective>
+        </MapsComponent>
+    );
+}
+export default SimpleMap;
