ae10e5de219ffcddc7f4214e1fb57cb45c121e56
AdamPostMontjoie <57641108+AdamPostMontjoie@users.noreply.github.com>
2025-11-01 19:12:41 -0400

    frontend setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 
-import './App.css'
 import MarkerTemplateMaps from './components/map'
+import Navbar from './components/navbar'
 
+import './index.css'
 function App() {
 
   return (
-    <div className="text-blue-500 text-xl font-bold">
-     Hi
-      <MarkerTemplateMaps/>
+    <div className="w-full min-h-screen bg-gray-100 overflow-x-hidden">
+      <Navbar/>
+
+        <div >
+          <MarkerTemplateMaps/>
+        </div>
     </div>
   )
 }
