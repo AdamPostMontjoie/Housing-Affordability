177dffc8f0e3c97324de8cff0b25e0d38c2e583f
AdamPostMontjoie <57641108+AdamPostMontjoie@users.noreply.github.com>
2025-11-11 21:18:22 -0500

    vercel config

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,9 @@ import os
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import re
-from dotenv import load_dotenv
 
 app = FastAPI()
 allowed_origin_regex = r"^(https?://localhost:\d+|https://housing-affordability.*\.vercel\.app)$"
-load_dotenv()
 
 
 app.add_middleware(
